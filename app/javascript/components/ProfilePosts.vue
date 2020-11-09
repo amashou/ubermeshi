5ef53fe97cd9710ff6666c6087a25bced9a6fafd
Shohei Amano <amasho@ShoheinoMac-mini.local>
2020-11-10 00:36:13 +0900

    投稿一覧のレイアウト修正

--- app/javascript/components/ProfilePosts.vue
+++ app/javascript/components/ProfilePosts.vue
@@ -6,11 +6,11 @@
                 <v-card class="mx-4 mb-4" color="#EEE" raised>
                     <v-img :src="post.food_picture.url" height="200px" cover></v-img>
                     <v-divider></v-divider>
-                    <v-card-title>{{post.title}}</v-card-title>
-                    <v-card-subtitle class="py-3">店名-{{post.restaurant_name}}</v-card-subtitle>
-                    <v-card-subtitle class="pt-0">住所-{{post.restaurant_address}}</v-card-subtitle>
+                     <v-card-title class="post-title pt-3">{{post.title}}</v-card-title>
+                    <v-card-subtitle class="py-3 restaurant-name">店名-{{post.restaurant_name}}</v-card-subtitle>
+                    <v-card-subtitle class="py-0 restaurant_address">住所-{{post.restaurant_address}}</v-card-subtitle>
                     <v-divider></v-divider>
-                    <v-card-text>{{post.description}}</v-card-text>
+                    <v-card-text class="post-description">{{post.description}}</v-card-text>
                     <v-chip color="accent" right class="ma-3" outlined route :to="{ name: 'PostDetail', params: {id: post.id} }">もっと見る</v-chip>
                     <v-btn outlined fab @click="destroy(post)" small color="pink"><v-icon>mdi-trash-can-outline</v-icon></v-btn>
                 </v-card>
@@ -54,3 +54,15 @@ export default {
     }
 }
 </script>
+
+<style scoped>
+.post-title{
+    height: 90px;
+}
+.restaurant-name{
+    height: 60px;
+}
+.restaurant_address {
+    height: 50px;
+}
+</style>
